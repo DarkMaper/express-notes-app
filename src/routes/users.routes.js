1c23ee8cbd7080ecc019c0f69f0ba67d70183633
Luis Orozco <lorozco@petroprix.com>
2022-03-30 08:58:38 +0200

    chore: Cambio de signin a método propio.

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const { renderLoginForm, renderSignupForm, logout } = require('../controllers/users.controllers');
+const { renderLoginForm, renderSignupForm, logout, signup } = require('../controllers/users.controllers');
 
 router.get('/signin', renderLoginForm);
 router.post('/signin', passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/signin',
+    failureFlash: true
 }));
 router.get('/signup', renderSignupForm);
-router.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/signin',
-    failureMessage: '/signup'
-}))
+router.post('/signup', signup);
 
 router.get('/logout', logout);
 
