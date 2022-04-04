8e2e6885d48539807ca22d93919d1a4069af80d0
Luis Orozco <lorozco@petroprix.com>
2022-04-04 08:46:10 +0200

    chore: Cambio isLoggedIn. Ahora usará el método req.isAuthenticated()

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,8 +1,8 @@
 const isLoggedIn = (req, res, next) => {
-    const isLogged = req.user ? true : false;
 
-    if(!isLogged) return res.redirect('/signin');
-    next();
+    if(req.isAuthenticated()) return next();
+    res.redirect('/signin');
+    
 }
 
 module.exports = {
