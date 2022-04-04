6a6c805b1c0476984296ff0f1f10e33969c3223a
Luis Orozco <lorozco@petroprix.com>
2022-04-04 08:56:07 +0200

    chore: Eliminado código sin usar

--- src/database.js
+++ src/database.js
@@ -1,18 +1,3 @@
-/* const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize(process.env.DB_URI);
-
-(async function() {
-    try {
-        await sequelize.authenticate();
-        await sequelize.sync({ force: false });
-        console.log('Connected to database');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-})()
-
-module.exports = sequelize; */
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Database is connected'))
